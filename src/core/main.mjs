72114a3acb6eaabb40cfede3eee4d8924f1805e1
TiagoMarinho <tiagomeraki@gmail.com>
2025-02-12 02:35:26 +0100

    fix improper import, add simple test

--- src/core/main.mjs
+++ src/core/main.mjs
@@ -1,7 +1,11 @@
-import Character from './entities/Character.mjs'
-import roleData from '../data/roleData.json' assert { type: `json` }
+import Character from '../entities/Character.mjs'
+//import roleData from '../data/roleData.json' assert { type: `json` }
 
 const main = async _ => {
-	// TODO
+	const myChar = new Character()
+	myChar.level.emitter.on("level changed", _ => {
+		console.log("LEVEL UP!!!")
+	})
+	myChar.level.experience += 10000
 }
 main()
